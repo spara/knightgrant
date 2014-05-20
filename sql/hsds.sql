7cf46e582b21752387b01479b8788c494b41085d
spara <sophia.parafina@gmail.com>
2014-05-20 17:56:24 -0500

    updated sql

--- sql/hsds.sql
+++ sql/hsds.sql
@@ -1,9 +1,8 @@
-CREATE TABLE IF NOT EXISTS organization (
+CREATE TABLE organization (
   organization_id INT PRIMARY KEY NOT NULL,
   organization_name VARCHAR(256) NULL,
   fein VARCHAR(64) NULL,
   aka VARCHAR(256) NULL,
-  urls TEXT NULL,
   legal_status VARCHAR(256) NULL,
   year_of_incorporation TIMESTAMP NULL,
   licenses TEXT NULL,
@@ -16,9 +15,9 @@ CREATE TABLE location (
   name VARCHAR(256) NULL,
   aka VARCHAR(256) NULL,
   accessibility TEXT NULL,
-  coordinates TEXT NULL,
+  latitude numeric,
+  longitude numeric,
   description TEXT NULL,
-  emails VARCHAR(45) NULL,
   location_hours TEXT NULL,
   location_languages TEXT NULL,
   short_desc TEXT NULL,
@@ -28,20 +27,19 @@ CREATE TABLE service (
   service_id INT PRIMARY KEY NOT NULL,
   organization_id INT NULL,
   location_id INT NULL,
-  audience VARCHAR(45) NULL,
+  audience TEXT NULL,
   description TEXT NULL,
-  eligibility VARCHAR(45) NULL,
-  fees VARCHAR(45) NULL,
-  funding_sources TEXT NULL,
+  eligibility TEXT NULL,
+  fees TEXT NULL,
   keywords TEXT NULL,
-  how_to_apply VARCHAR(45) NULL,
-  name VARCHAR(64) NULL,
+  how_to_apply TEXT NULL,
+  name VARCHAR(255) NULL,
   service_areas TEXT NULL,
   short_desc TEXT NULL,
   urls TEXT NULL,
   service_languages TEXT NULL,
   service_hours TEXT NULL,
-  wait VARCHAR(256) NULL,
+  wait VARCHAR(255) NULL,
   method_of_payment TEXT NULL);
 
 CREATE TABLE address (
@@ -81,3 +79,103 @@ CREATE TABLE funding (
   organization_id INT PRIMARY KEY NOT NULL,
   service_id INT NOT NULL,
   source TEXT NULL);
+ 
+-- Converting an iCarol resource table to HSDS tables
+
+INSERT INTO organization(
+  organization_id,
+  organization_name,
+  fein,
+  aka,
+  legal_status,
+  year_of_incorporation,
+  licenses,
+  accreditation )
+SELECT
+  resourceagencynum,
+  publicname,
+  fein,
+  alternatename,
+  legalstatus,
+  to_timestamp(yearincorporated,'YYYY'),
+  licenseaccreditation,
+  licenseaccreditation
+FROM resources
+WHERE resources.taxonomylevelname = 'Agency'; 
+
+-- populate location table
+
+INSERT INTO location(
+  location_id,
+  organization_id,
+  name,
+  aka,
+  accessibility,
+  latitude,
+  longitude,
+  location_hours,
+  location_languages,
+  transportation
+)
+SELECT
+  resourceagencynum,
+  parentagencynum,
+  publicname,
+  alternatename,
+  disabilitiesaccess,
+  cast(latitude AS numeric),
+  cast(longitude AS numeric),
+  hoursofoperation,
+  languagesoffered,
+  busserviceaccess
+FROM resources
+WHERE resources.taxonomylevelname = 'Site';
+
+
+UPDATE location
+SET description = resources.agencydescription
+FROM resources
+WHERE 
+  resources.parentagencynum = location.organization_id
+
+SELECT location_id,name,description FROM location;
+
+-- populate program table
+
+INSERT INTO service (
+  service_id,
+  organization_id,
+  eligibility,
+  fees,
+  keywords,
+  how_to_apply,
+  name,
+  service_areas,
+  service_languages,
+  service_hours,
+  wait
+) 
+SELECT
+  resourceagencynum,
+  parentagencynum,
+  eligibility,
+  feestructuresource,
+  taxonomyterm,
+  applicationprocess,
+  publicname,
+  coveragearea,
+  languagesoffered,
+  hoursofoperation,
+  normalwaittime
+FROM resources
+WHERE resources.taxonomylevelname = 'Program';
+
+UPDATE service
+SET description = resources.agencydescription
+FROM resources
+WHERE 
+  resources.parentagencynum = service.organization_id
+
+SELECT service_id,name,description FROM service;
+
+
